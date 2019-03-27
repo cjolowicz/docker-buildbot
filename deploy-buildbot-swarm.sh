b7467d9080a22cfa65e11d31c39f460b9939aaa4
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-03-27 16:46:30 +0100

    Add script to deploy buildbot swarm

---
+++ deploy-buildbot-swarm.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ $# -gt 0 ]
+then
+    case $1 in
+        '--remove' | '--rm')
+            set +e -x
+
+            docker stack rm buildbot
+            docker-machine rm -f worker
+            docker-machine rm -f manager
+
+            exit
+            ;;
+    esac
+
+    echo "unrecognized option \`$1'" >&2
+    exit 1
+fi
+
+set -x
+
+docker-machine create --driver virtualbox manager
+docker-machine create --driver virtualbox worker
+
+manager_ip=$(docker-machine ls --format='{{.URL}}' --filter='name=manager' |
+         sed -e 's,^tcp://,,' -e 's/:[^:]*$//')
+
+eval $(docker-machine env manager)
+
+docker swarm init --advertise-addr $manager_ip
+
+token=$(docker swarm join-token --quiet worker)
+
+(
+    eval $(docker-machine env worker)
+    docker swarm join --token $token $manager_ip:2377
+)
+
+docker stack deploy -c docker-compose.yml buildbot
