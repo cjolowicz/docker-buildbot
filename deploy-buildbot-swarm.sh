bbe54057d3f3b1d430075a25a0bdd75241458a40
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-03-28 16:59:14 +0100

    Clean up swarm deploy script

--- deploy-buildbot-swarm.sh
+++ deploy-buildbot-swarm.sh
@@ -2,41 +2,47 @@
 
 set -euo pipefail
 
-if [ $# -gt 0 ]
+function create() {
+    docker-machine create --driver virtualbox manager
+    docker-machine create --driver virtualbox worker
+
+    manager_ip=$(docker-machine ip manager)
+
+    eval $(docker-machine env manager)
+
+    docker swarm init --advertise-addr=$manager_ip
+
+    token=$(docker swarm join-token --quiet worker)
+
+    (
+        eval $(docker-machine env worker)
+        docker swarm join --token=$token $manager_ip:2377
+    )
+
+    docker stack deploy --compose-file=docker-compose.yml buildbot
+    docker service ls
+}
+
+function remove() {
+    docker-machine ssh manager "docker stack rm buildbot"
+    docker-machine rm -f worker
+    docker-machine rm -f manager
+}
+
+if [ $# -eq 0 ]
 then
+    set -x
+    create
+else
     case $1 in
         '--remove' | '--rm')
-            set +e -x
+            set -x +e
+            remove
+            ;;
 
-            docker-machine ssh manager "docker stack rm buildbot"
-            docker-machine rm -f worker
-            docker-machine rm -f manager
-
-            exit
+        *)
+            echo "unrecognized option \`$1'" >&2
+            exit 1
             ;;
     esac
-
-    echo "unrecognized option \`$1'" >&2
-    exit 1
 fi
-
-set -x
-
-docker-machine create --driver virtualbox manager
-docker-machine create --driver virtualbox worker
-
-manager_ip=$(docker-machine ip manager)
-
-eval $(docker-machine env manager)
-
-docker swarm init --advertise-addr $manager_ip
-
-token=$(docker swarm join-token --quiet worker)
-
-(
-    eval $(docker-machine env worker)
-    docker swarm join --token $token $manager_ip:2377
-)
-
-docker stack deploy -c docker-compose.yml buildbot
-docker service ls
