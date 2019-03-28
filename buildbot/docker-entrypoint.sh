21593d1783144f6fc29def8446fad606c4481142
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-03-28 17:25:04 +0100

    Do not bind-mount Docker daemon socket

--- buildbot/docker-entrypoint.sh
+++ buildbot/docker-entrypoint.sh
@@ -4,15 +4,4 @@ set -e
 
 buildbot upgrade-master .
 
-if [ ! -S /var/run/docker.sock ]
-then
-    echo "/var/run/docker.sock not found" >&2
-    exit 1
-fi
-
-gid=$(stat -c '%g' /var/run/docker.sock)
-
-adduser -h /var/lib/buildbot -D buildbot
-chown -R buildbot /var/lib/buildbot
-
-su-exec buildbot:$gid "$@"
+exec "$@"
