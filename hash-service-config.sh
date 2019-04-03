e22508082ba2b39433c7faee503431a85ace14a2
Claudio Jolowicz <mail@claudiojolowicz.com>
2019-04-03 11:34:49 +0200

    Add BUILDBOT_URL to service environment
    Configure the external URL at which the web interface is accessed
    using the BUILDBOT_URL environment variable. This corresponds to the
    `buildbotURL` entry in `BuildmasterConfig`.
    
    If this environment variable is set in the shell from which the stack
    is deployed, it will be exported to the service environment.
    
    If BUILDBOT_URL is unset but DOCKER_HOST is set, its value is used to
    provide a default for BUILDBOT_URL. Otherwise, `buildbotURL` defaults
    to `http://localhost:8010`.
    
    An incorrect buildbot URL results in errors in the web interface,
    because requests to the API violate the Same Origin Policy. An example
    would be the following message when triggering a build:
    
        "invalid origin"

--- hash-service-config.sh
+++ hash-service-config.sh
@@ -3,6 +3,13 @@
 # config names must be maximum 64 characters in length
 BUILDBOT_MASTER_CFG_HASH=$(sha256sum master.cfg | cut -c-16)
 
+if [ -z "$BUILDBOT_URL" -a -n "$DOCKER_HOST" ]
+then
+    BUILDBOT_URL=http://${DOCKER_HOST#*://}
+    BUILDBOT_URL=${BUILDBOT_URL%:*}:8010
+    echo "export BUILDBOT_URL=$BUILDBOT_URL"
+fi
+
 cat <<EOF
 export BUILDBOT_MASTER_CFG_HASH=$BUILDBOT_MASTER_CFG_HASH
 # Run this command to configure your shell:
