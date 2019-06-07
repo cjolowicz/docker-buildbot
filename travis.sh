5bd290302b9855e1ff7a4be0269d9e7b859842ae
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2019-06-07 16:01:31 +0200

    [CI] Determine correct branch name

--- travis.sh
+++ travis.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-make ci GIT_TAG="$TRAVIS_TAG"
+if [ -z "$TRAVIS_TAG" ]
+then
+    BRANCH="$TRAVIS_BRANCH"
+else
+    BRANCH=master
+fi
+
+make ci GIT_BRANCH="$BRANCH" GIT_TAG="$TRAVIS_TAG"
